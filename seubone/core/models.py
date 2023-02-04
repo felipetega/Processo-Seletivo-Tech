ab7ba4648a968329ac61cc2fdb54a2052afa124c
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2023-02-04 13:39:15 -0300

    create table tecido

--- seubone/core/models.py
+++ seubone/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+class Tecido(models.Model):
 
-# Create your models here.
+  choices_tipo = (
+  ('LINHO', 'Linho'),
+  ('CAMURÇA', 'Camurça'),
+  ('MESCLA', 'Mescla'),
+  ('MOLETOM', 'Moletom'),
+  )
+
+  choices_cores=(
+  ('AZUL', 'Azul'),
+  ('AMARELO', 'Amarelo'),
+  ('VERMELHO', 'Vermelho'),
+  ('VERDE', 'Verde'),
+  )
+
+  tipo = models.CharField(max_length=50, choices=choices_tipo)
+  cor = models.CharField(max_length=50, choices=choices_cores)
+  metros = models.IntegerField()
